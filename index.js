2a1bb9c5d569816aa6c2bf69ee9d5bac7176265b
Seda Ünal <sedaylmz06@outlook.com>
2022-10-30 22:18:36 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+  
+  // method: "POST" is missing from the object below
+//   const configurationObject = {
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       console.log(object);
+//     })
+//     .catch(function (error) {
+//       alert("Bad things! Ragnarők!");
+//       console.log(error.message);
+//     });
+//   fetch("http://localhost:3000/dogs", configurationObject);
+
+var form=document.getElementById('form')
+
+form.addEventListener('submit', function(e){
+ e.preventDefault()
+
+ var name=document.getElementById('name').value
+ var body=document.getElementById('body').value
+
+ fetch('http://localhost:3000/users', {
+  method: 'POST',
+  body: JSON.stringify({
+    title:name,
+    body:body,
+
+  }),
+  headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        }
+  })
+  .then(function(response){ 
+  return response.json()})
+  .then(function(data)
+  {console.log(data)
+  title=document.getElementById("title")
+  body=document.getElementById("bd")
+  title.innerHTML = data.title
+  body.innerHTML = data.body  
+}).catch(error => console.error('Error:', error)); 
+});
+
+//   function submitData(name,email) {
+
+//   }
+
+  
